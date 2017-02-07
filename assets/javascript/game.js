49e5cf9c0f136b3a01a86edaba4d204137bce551
alex nichols <alex nichols>
2017-02-07 15:29:55 -0800

    javascript update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,9 +2,20 @@
 $(document).ready(function() {
 
 	var number = parseInt("");
-	var totalScore = "";
+	var totalScore = 0;
+	var wins = 0;
+	$("#wins").html(wins);
+	var losses = 0;
+	$("#losses").html(losses);
 	var lockGame = false;
 	var computersChoice = Math.floor(Math.random() * 120) + 19;
+
+	function resetGame() {
+
+		computersChoice = Math.floor(Math.random() * 120) + 19;
+		number = computersChoice;
+		totalScore = 0;
+	}
 
 	number = computersChoice;
 
@@ -16,11 +27,22 @@ $(document).ready(function() {
 
 		usersChoice = parseInt(usersChoice);
 
-		$("#total-score").html(usersChoice);
+		$("#total-score").html(totalScore += usersChoice);
 
-		
+		if(totalScore > computersChoice) {
 
-		
+			losses++;
+			$("#losses").html(losses);
+
+		}
+
+		if(totalScore === computersChoice) {
+
+			wins++;
+			$("#wins").html(wins);
+
+		}
+
 	});
 
 	
